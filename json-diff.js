0b7aff5194f6c34d559692eef9d00dd9bcd7d5be
pkafel <piotr.kafel@gmail.com>
2015-04-02 19:03:09 +0200

    Correct way of comparing two Arrays

--- json-diff.js
+++ json-diff.js
@@ -1,8 +1,18 @@
 function areJsonsEqual(left, right) {
+
+  function areArraysEqual(left, right) {
+    if(left.length !== right.length) return false;
+
+    for(var i = left.length; i--;) {
+      if(left[i] !== right[i]) return false;
+    }
+    return true;
+  }
+
   var leftJson = JSON.parse(left);
   var rightJson = JSON.parse(right);
   // can only be arrays or objects
-  if(leftJson instanceof Array && rightJson instanceof Array) return leftJson.length === rightJson.length;
+  if(leftJson instanceof Array && rightJson instanceof Array) return areArraysEqual(left, right);
   else if(!(leftJson instanceof Array) && !(rightJson instanceof Array)) return Object.keys(leftJson).length === Object.keys(rightJson).length;
   else return false;
 }
--- test/spec/json-diff-spec.js
+++ test/spec/json-diff-spec.js
@@ -11,4 +11,12 @@ describe("Json diff", function() {
   it("Array and Json objest should not be equal", function() {
     expect(areJsonsEqual("[]", "{}")).toBe(false);
   });
+  
+  it("Two arrays with the same elements but in different order should not be equal ", function() {
+    expect(areJsonsEqual("[1,2,3]", "[1,3,2]")).toBe(false);
+  });
+  
+  it("Two arrays with the same elements in the same order should be equal", function() {
+    expect(areJsonsEqual("[3,1,2]", "[3,1,2]")).toBe(true);
+  });
 });
