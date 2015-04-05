82d7d11e678d3c4b2f0887668a5eeb646f58901f
pkafel <piotr.kafel@gmail.com>
2015-04-05 17:42:43 +0200

    First draft of representation and simple comparision of very small JSONs

--- json-diff.js
+++ json-diff.js
@@ -25,3 +25,63 @@ function areJsonsEqual(left, right) {
   else if(!(leftJson instanceof Array) && !(rightJson instanceof Array)) return areJsonObjectsEqual(leftJson, rightJson);
   else return false;
 }
+
+function getDiffRepresentation(left, right) {
+
+  function getArraysDiff(left, right) {
+    var result = [];
+    for(var i = 0;i < left.length;i++) {
+      if(left[i] !== right[i]) {
+	result.push(createDiffEntry(left[i], "ADD"));
+	result.push(createDiffEntry(right[i], "REMOVE"));
+      } else {
+	result.push(createDiffEntry(left[i], "NONE"));
+      }
+    }
+
+    if(left.length < right.length) {
+      for(var i = left.length;i < right.length ;i++) {
+	result.push(createDiffEntry(right[i], "REMOVE"));
+      }
+    }
+
+    return result;
+  }
+
+  function getJsonsDiff(left, right) {
+    var result = {};
+
+    for(var key in left) {
+      if(!right.hasOwnProperty(key)) result[key] = createDiffEntry(left[key], "ADD");
+      else if(left[key] !== right[key]) result[key] = createReplaceEntry(left[key], right[key]);
+      else result[key] = createDiffEntry(left[key], "NONE");
+    }
+
+    for(var key in right) {
+      if(!result.hasOwnProperty(key)) result[key] = createDiffEntry(right[key], "REMOVE");
+    }
+    return result;
+  }
+
+  function createDiffEntry(val, diffType) {
+    return {
+      "value": val,
+      "diffType": diffType
+    };
+  }
+
+  function createReplaceEntry(val, oldVal) {
+    return {
+      "value": val,
+      "oldValue": oldVal,
+      "diffType": "REPLACE"
+    };
+  }
+
+  var leftJson = JSON.parse(left);
+  var rightJson = JSON.parse(right);
+
+  if(leftJson instanceof Array && rightJson instanceof Array) return getArraysDiff(leftJson, rightJson);
+  else if(!(leftJson instanceof Array) && !(rightJson instanceof Array)) return getJsonsDiff(leftJson, rightJson);
+  else throw "Functionality not yet implemented !";
+}
--- test/spec/json-diff-spec.js
+++ test/spec/json-diff-spec.js
@@ -32,3 +32,34 @@ describe("Json diff", function() {
     expect(areJsonsEqual("{\"key1\": 1234}", "{\"key1\": 12}")).toBe(false);
   });
 });
+
+describe("Get Json diff representation", function() {
+
+  it("For two arrays with different value should return correct diff", function() {
+    var result = getDiffRepresentation("[1,2]", "[4,5]");
+    expect(result[0].value).toEqual(1);
+    expect(result[0].diffType).toEqual("ADD");
+    expect(result[1].value).toEqual(4);
+    expect(result[1].diffType).toEqual("REMOVE");
+    expect(result[2].value).toEqual(2);
+    expect(result[2].diffType).toEqual("ADD");
+    expect(result[3].value).toEqual(5);
+    expect(result[3].diffType).toEqual("REMOVE");
+  });
+
+  it("For two arrays with the same values should return correct diff", function() {
+    var result = getDiffRepresentation("[1,2]", "[1,2]");
+    expect(result[0].value).toEqual(1);
+    expect(result[0].diffType).toEqual("NONE");
+    expect(result[1].value).toEqual(2);
+    expect(result[1].diffType).toEqual("NONE");
+  });
+
+  it("For two the same flat JSONs should return object without any differences", function() {
+    var result = getDiffRepresentation("{\"key1\": 123, \"key2\": \"some value\"}", "{\"key2\": \"some value\", \"key1\": 123}");
+    expect(result["key1"].value).toEqual(123);
+    expect(result["key1"].diffType).toEqual("NONE");
+    expect(result["key2"].value).toEqual("some value");
+    expect(result["key2"].diffType).toEqual("NONE");
+  });
+});
